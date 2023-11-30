d9f8b7e84d5980895055c74d3ec3d4fe7d6b7429
Mezaiah <samueldelcastillo1710@gmail.com>
2023-11-30 03:55:57 -0600

    Primera prueba de conexión
    se logró.

--- MultiPPractica/Assets/Scripts/ConnectToServer.cs
+++ MultiPPractica/Assets/Scripts/ConnectToServer.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviour
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // Update is called once per frame
